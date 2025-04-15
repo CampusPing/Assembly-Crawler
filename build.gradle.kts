bb1c6b05491bc24af144cf575062e8a284aab278
buna <tmdgh1592@naver.com>
2025-04-16 01:13:12 +0900

    구성 : 라이브러리 배포를 위한 build.gradle 스크립트 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("maven-publish")
 }
 
 repositories {
@@ -19,4 +20,15 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create("maven-public", MavenPublication::class) {
+            groupId = "com.campusping"
+            artifactId = "assembly-crawler"
+            version = "1.0.0-rc1"
+            from(components.getByName("java"))
+        }
+    }
 }
